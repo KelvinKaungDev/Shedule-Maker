f316acb9d2a1a7533c5fa17bc68a3f35204fb251
Kelvin Gao <u6511753@au.edu>
2023-08-26 15:24:56 +0700

    Initial Commit

---
+++ SchduleForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchduleForm
+//
+//  Created by Kelvin Gao  on 26/8/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
